1d666b5b2edee978b82a305839b3d14e92b1af41
Michaela Mattes <michaela.mattes@t-systems.com>
2023-03-14 17:19:30 +0100

    test

---
+++ tests/versions.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "registry.terraform.io/hashicorp/azurerm"
+      version = "=3.42.0"
+    }
+  }
+  required_version = "=1.3.9"
+}
