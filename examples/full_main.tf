359123c25a40d772495a776dc2517cc4e26358a3
Michaela Mattes <michaela.mattes@t-systems.com>
2023-03-14 15:57:46 +0100

    test

---
+++ examples/full_main.tf
@@ -0,0 +1,15 @@
+module "container" {
+  source = "registry.terraform.io/T-Systems-MMS/container/azurerm"
+  container_registry = {
+    crmms = {
+      location            = "westeurope"
+      resource_group_name = "rg-mms-github"
+      sku                 = "Premium"
+      tags = {
+        project     = "mms-github"
+        environment = terraform.workspace
+        managed-by  = "terraform"
+      }
+    }
+  }
+}
